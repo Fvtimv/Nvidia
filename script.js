cdb75c4db341c652cb805adc8bb9ced1f7b0d3bd
Fvtimv <fatima.hilmi@hotmail.com>
2023-01-05 16:54:10 +0100

    First time using Javascript and getting started on it

---
+++ script.js
@@ -0,0 +1,12 @@
+var modal = document.getElementById("modal");
+
+var modalImage = document.getElementById("myImg1");
+
+var image = document.getElementById("myImg");
+
+image.onclick = function() {
+   modal.style.display = "block";
+    // modalImage.src = this.src;
+
+}
+
