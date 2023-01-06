27506a5aca98733a8cf767d3534eb8819152afef
Fvtimv <fatima.hilmi@hotmail.com>
2023-01-06 15:23:04 +0100

    Made the modal images for the section gallery after many failed attempts

--- script.js
+++ script.js
@@ -1,17 +1,54 @@
-var modal = document.getElementById("modal");
+// var modal = document.getElementsByClassName("modal");
 
-var modalImage = document.getElementById("myImg1");
+// var modalImage = document.getElementById("myImg1");
 
-var image = document.getElementById("myImg");
+// var image = document.getElementsByClassName("myImages");
 
-image.onclick = function() {
-   modal.style.display = "block";
-modalImage.src = this.src;
-caption.innerHTML = this.alt;
-}
+// var captionText = document.getElementById("caption")
 
-var span = document.getElementsByClassName ("close") [0];
+// image.onclick = function() {
+//    modal.style.display = "block";
+// modalImage.src = this.src;
+// captionText.innerHTML = this.alt;
+// }
 
-span.onclick = function() {
+
+// var span = document.getElementsByClassName ("close") [0];
+
+// span.onclick = function() {
+//     modal.style.display = "none";
+// }
+
+var modal = document.getElementById("myModal");
+
+var images = document.querySelectorAll(".myImages");
+
+var modalImg = document.getElementById("img1");
+
+var captionText = document.getElementById("caption");
+
+// for (var i = 0; i < images.length; i++) {
+//     var img = images[i];
+
+
+// img.onclick = function () {
+//     modal.style.display = "block";
+//     modalImg.src = this.src;
+//     captionText.innerHTML = this.alt;
+// }
+// }
+
+
+images.forEach(function(image){
+    image.addEventListener("click", function (){
+        modal.style.display = "block";
+        modalImg.src = this.src;
+        captionText.innerHTML = this.alt;
+    })
+}) ;
+
+var span = document.getElementsByClassName("close")[0];
+
+span.addEventListener("click", function(){
     modal.style.display = "none";
-}
+} )
